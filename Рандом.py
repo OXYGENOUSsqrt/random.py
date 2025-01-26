58696854359328ccca6f44936a325b76c7b7e0cc
Oxygenous <94700661+OXYGENOUSsqrt@users.noreply.github.com>
2025-01-26 15:44:48 +0500

    Add files via upload

---
+++ Рандом.py
@@ -0,0 +1,10 @@
+from random import *
+
+a = randrange (10, 45, 5) # шаг 5
+print (a)
+b = randint (0, 1) # 0 или 1
+print (b)
+v = random ()   # любое число до 1, 0.?
+print (v)
+n = uniform (5, 10) # от 5 с точкой плавающей запятой
+print (n)
